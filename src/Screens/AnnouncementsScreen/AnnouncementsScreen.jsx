986e5af78ec0074f7d5257d948b30fd36a9b41ff
valeri.h <valerihernandez712@gmail.com>
2026-03-07 17:33:07 -0300

    ajuste

--- src/Screens/AnnouncementsScreen/AnnouncementsScreen.jsx
+++ src/Screens/AnnouncementsScreen/AnnouncementsScreen.jsx
@@ -1,18 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./AnnouncementsScreen.css";
 
 
-export default function AnnouncementsScreen({ onClose }) {
+export default function AnnouncementsScreen() {
+
+  const navigate = useNavigate();
   return (
     <div className="announcement-overlay">
-
       <div className="announcement-card">
-
         <h2>Consigue nuevos clientes con anuncios</h2>
-
         <p>Crea anuncios que se muestran en Facebook e Instagram y haz que
           las personas se comuniquen contigo a través de WhatsApp.</p>
-
         <ul>
           <li>Llega al público local</li>
           <li>Controla tu presupuesto</li>
@@ -21,13 +20,9 @@ export default function AnnouncementsScreen({ onClose }) {
 
         <div className="announcement-buttons">
 
-          <button className="cancel-btn" onClick={onClose}>
-            Cancelar
-          </button>
+          <button className="cancel-btn" onClick={() => navigate("/chats")}>Cerrar</button>
 
-          <button className="continue-btn">
-            Continuar en Facebook
-          </button>
+          <button className="continue-btn">Continuar en Facebook</button>
 
         </div>
       </div>
